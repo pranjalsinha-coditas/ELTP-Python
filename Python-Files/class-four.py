da2841a6ecd6db2392bf13bcb285dde738369c8a
Pranjal Sinha <158136420+pranjalsinha-coditas@users.noreply.github.com>
2024-03-11 14:42:54 +0530

    reafactor:changes

---
+++ Python-Files/class-four.py
@@ -0,0 +1,79 @@
+# ClassWork => 11th March, 2024
+
+numbers = [1, 2, 3, 4, 5]
+
+#make an array with different dictionaries having different arrays
+Array = [
+    {"name" : "Pranjal"}, 
+    {"name" : "Shambhu"}, 
+    {"name" : "Ravi"}
+]
+
+#delete the second dictionary
+Array.pop(1)
+print(Array)
+
+#add the fourth dictionary with another name
+Array.insert(4, {
+    "name" : "Nitya"
+})
+print(Array)
+
+People = [
+    {"name" : "A"}, 
+    {"name" : "B"}, 
+    {"name" : "C"}, 
+    {"name" : "D"}
+]
+
+People.remove({"name" : "A"})
+print(People)
+
+three_odd_numbers = [3, 5, 7, 9]
+three_even_numbers = [2, 4, 6, 8]
+
+merged_array = [*three_odd_numbers, *three_even_numbers]
+print(merged_array)
+
+print(three_odd_numbers.__len__())
+print(len(three_odd_numbers))
+
+List = ['Mathematics', 'Chemistry', 1997, 2000, 'Mathematics']
+List.append(20544)
+List.insert(2, 10087)
+print(List)
+print(len(List))
+
+# min, max, sum, pop, del, remove, count
+# Reverse flag is set for true while we use sort
+
+# cloning or cppying list using 
+# => slicing technique
+
+original_list = [4, 8, 2, 10, 15, 18]
+cloned_list_slice = original_list[:]
+cloned_list_extend = original_list.copy()
+
+import copy 
+original_list = [1, 2, [3, 5], 4]
+cloned_list_shallow_copy_module = copy.copy(original_list)
+
+cloned_list_comprehension = [i for i in original_list]
+
+cloned_list_append = []
+for item in original_list:
+    cloned_list_append.append(item)
+
+cloned_list_with_enumerate = original_list.copy()
+
+original_list_copy_module = [1, 2, [3, 5], 4] #deepcopy
+cloned_list = copy.deepcopy(original_list)
+
+cloned_list_enumerate = [i for a, i in enumerate(original_list_copy_module)]
+
+# cloned_list_map = list(map(int, original_list_copy_module))
+
+cloned_list_len = original_list[slice(len(original_list_copy_module))]
+
+
+
